8276e45026a8171e7abc749d4da59751e90d90a8
RaulResendeDev <raulresendeti@gmail.com>
2023-10-27 08:06:49 -0300

    criando sessao criptografada para armazenar token jwt

--- app/src/main/java/br/com/fiap/startupone/config/UserSessionManager.kt
+++ app/src/main/java/br/com/fiap/startupone/config/UserSessionManager.kt
@@ -3,17 +3,35 @@ package br.com.fiap.startupone.config
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.navigation.NavHostController
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import br.com.fiap.startupone.model.UsuarioLogadoDto
 
 class UserSessionManager private constructor(context: Context) {
 
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences("user_session", Context.MODE_PRIVATE)
 
+    private val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val encryptedSharedPreferences = EncryptedSharedPreferences.create(
+        context,
+        "user_session_encrypted",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
     fun saveUserSession(usuarioLogadoDto: UsuarioLogadoDto) {
         with(sharedPreferences.edit()) {
-            putInt("id", usuarioLogadoDto.IdUsuario)
             putString("nome", usuarioLogadoDto.Nome)
             putString("email", usuarioLogadoDto.Email)
+            apply()
+        }
+
+        with(encryptedSharedPreferences.edit()) {
+            putString("token", usuarioLogadoDto.Token)
             apply()
         }
     }
@@ -24,12 +42,14 @@ class UserSessionManager private constructor(context: Context) {
 
         val nome = sharedPreferences.getString("nome", null)
         val email = sharedPreferences.getString("email", null)
+        val token = encryptedSharedPreferences.getString("token", null)
 
-        return UsuarioLogadoDto(id, nome, email)
+        return UsuarioLogadoDto(id, nome, email, token)
     }
 
     fun clearUserSession() {
         sharedPreferences.edit().clear().apply()
+        encryptedSharedPreferences.edit().clear().apply()
     }
 
     fun isLoggedIn(): Boolean {
--- app/src/main/java/br/com/fiap/startupone/model/UsuarioLogadoDto.kt
+++ app/src/main/java/br/com/fiap/startupone/model/UsuarioLogadoDto.kt
@@ -3,5 +3,6 @@ package br.com.fiap.startupone.model
 data class UsuarioLogadoDto(
     val IdUsuario: Int,
     val Nome: String?,
-    val Email: String?
+    val Email: String?,
+    val Token: String?
 )
