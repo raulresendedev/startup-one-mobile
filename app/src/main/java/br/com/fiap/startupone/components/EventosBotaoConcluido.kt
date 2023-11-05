38cd73b8abf0df97f2b156564fb37ada27aabb57
RaulResendeDev <raulresendeti@gmail.com>
2023-11-05 12:09:52 -0300

    adicionando animacao conclusao evento

--- app/src/main/java/br/com/fiap/startupone/components/EventosBotaoConcluido.kt
+++ app/src/main/java/br/com/fiap/startupone/components/EventosBotaoConcluido.kt
@@ -1,5 +1,8 @@
 package br.com.fiap.startupone.components
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -9,6 +12,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import br.com.fiap.startupone.model.EventosMarcadosDto
@@ -16,12 +23,25 @@ import br.com.fiap.startupone.model.EventosMarcadosDto
 @Composable
 fun EventosBotaoConcluido(evento: EventosMarcadosDto, onToggleCompletion: (EventosMarcadosDto) -> Unit) {
     val icon = Icons.Default.CheckCircle
-    val borderColor = MaterialTheme.colorScheme.onSurface.copy(.7f)
+    val borderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
     val borderWidth = 2.dp
     val backgroundColor = Color.Transparent
 
+    var concluido by remember { mutableStateOf(evento.concluido) }
+
+    val iconOpacity by animateFloatAsState(
+        targetValue = if (concluido) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 150,
+            easing = FastOutSlowInEasing
+        )
+    )
+
     IconButton(onClick = {
-        onToggleCompletion(evento)
+
+        concluido = !concluido
+
+        onToggleCompletion(evento.copy(concluido = concluido))
     }) {
         Surface(
             color = backgroundColor,
@@ -30,9 +50,10 @@ fun EventosBotaoConcluido(evento: EventosMarcadosDto, onToggleCompletion: (Event
         ) {
             Icon(
                 imageVector = icon,
-                contentDescription = if (evento.concluido) "Desmarcar" else "Marcar como concluído",
-                tint = MaterialTheme.colorScheme.primary
+                contentDescription = if (concluido) "Desmarcar" else "Marcar como concluído",
+                tint = MaterialTheme.colorScheme.primary.copy(alpha = iconOpacity)
             )
         }
     }
 }
+
